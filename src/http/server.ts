14884d08be9cc741114584994a3691deb27fd6bf
natanaelvich <taelima1997@gmail.com>
2024-02-20 19:36:56 -0300

    Add routes and error handling to HTTP server

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,26 @@
 import { Elysia } from 'elysia'
+import { registerRestaurante } from './routes/register-restaurante'
+import { sendAuthLink } from './routes/send-auth-link'
+import { authenticateFromLink } from './routes/authenticate-from-link'
 
 const app = new Elysia()
+.use(registerRestaurante)
+.use(sendAuthLink)
+.use(authenticateFromLink)
+.onError(({ code, error, set }) => {
+  switch (code) {
+    case 'VALIDATION': {
+      set.status = error.status
+
+      return error.toResponse()
+    }
+    default: {
+      console.error(error)
+
+      return new Response(null, { status: 500 })
+    }
+  }
+})
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
