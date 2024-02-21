a29cd92ae261a9ae2f96021dae0d79c7bb215454
natanaelvich <taelima1997@gmail.com>
2024-02-21 20:38:35 -0300

    Add new routes for sign-out, profile, managed restaurante, order details, and order approval

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,11 +2,21 @@ import { Elysia } from 'elysia'
 import { registerRestaurante } from './routes/register-restaurante'
 import { sendAuthLink } from './routes/send-auth-link'
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { signOut } from './routes/sign-out'
+import { getProfile } from './routes/get-profile'
+import { getManagedRestaurante } from './routes/get-managed-restaurante'
+import { getOrderDetails } from './routes/get-order-details'
+import { approveOrder } from './routes/approve-order'
 
 const app = new Elysia()
 .use(registerRestaurante)
 .use(sendAuthLink)
 .use(authenticateFromLink)
+.use(signOut)
+.use(getProfile)
+.use(getManagedRestaurante)
+.use(getOrderDetails)
+.use(approveOrder)
 .onError(({ code, error, set }) => {
   switch (code) {
     case 'VALIDATION': {
