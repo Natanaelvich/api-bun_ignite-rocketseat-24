e5854fa2f8285d68b8759408b3ffba7310f44b8a
natanaelvich <taelima1997@gmail.com>
2024-02-24 08:53:40 -0300

    Add new routes for order management

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,6 +7,16 @@ import { getProfile } from './routes/get-profile'
 import { getManagedRestaurante } from './routes/get-managed-restaurante'
 import { getOrderDetails } from './routes/get-order-details'
 import { approveOrder } from './routes/approve-order'
+import { cancelOrder } from './routes/cancel-order'
+import { deliverOrder } from './routes/deliver-order'
+import { dispatchOrder } from './routes/dispatch-order'
+import { getDailyReceiptInPeriod } from './routes/get-daily-receipt-in-period'
+import { getDayOrdersAmount } from './routes/get-day-orders-amount'
+import { getMonthCanceledOrdersAmount } from './routes/get-month-canceled-orders-amount'
+import { getMonthOrdersAmount } from './routes/get-month-orders-amount'
+import { getMonthReceipt } from './routes/get-month.receipt'
+import { getOrders } from './routes/get-orders'
+import { getPopularProducts } from './routes/get-popular-products'
 
 const app = new Elysia()
 .use(registerRestaurante)
@@ -17,12 +27,25 @@ const app = new Elysia()
 .use(getManagedRestaurante)
 .use(getOrderDetails)
 .use(approveOrder)
+.use(cancelOrder)
+.use(deliverOrder)
+.use(dispatchOrder)
+.use(getOrders)
+.use(getMonthReceipt)
+.use(getDayOrdersAmount)
+.use(getMonthOrdersAmount)
+.use(getMonthCanceledOrdersAmount)
+.use(getPopularProducts)
+.use(getDailyReceiptInPeriod)
 .onError(({ code, error, set }) => {
   switch (code) {
     case 'VALIDATION': {
       set.status = error.status
 
       return error.toResponse()
+    }
+    case 'NOT_FOUND': {
+        return new Response(null, { status: 404 })
     }
     default: {
       console.error(error)
